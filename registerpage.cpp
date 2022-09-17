faba763b2b190c0051399d21ac2ceeb0f8b69091
Shashinoor Ghimire <shashinoorghimire13@gmail.com>
2022-09-17 21:17:30 +0545

    backup

--- registerpage.cpp
+++ registerpage.cpp
@@ -1,5 +1,7 @@
 #include "registerpage.h"
 #include "ui_registerpage.h"
+#include "loginpage.h"
+#include <QMessageBox>
 
 registerpage::registerpage(QWidget *parent) :
     QDialog(parent),
@@ -12,3 +14,15 @@ registerpage::~registerpage()
 {
     delete ui;
 }
+
+void registerpage::on_pushButton_clicked()
+{
+    if(ui->radioButton->isChecked())
+    {
+        loginpage login;
+        hide();
+        login.exec();
+    }
+    else QMessageBox::warning(this,"Warnning!","You have not aggred to out terms and conditions.");
+}
+
--- registerpage.h
+++ registerpage.h
@@ -15,6 +15,9 @@ public:
     explicit registerpage(QWidget *parent = nullptr);
     ~registerpage();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::registerpage *ui;
 };
